e481d5c43e4d119f63ac1cd3e5426b5e1cbe7f1a
Vova <vova77gh@gmail.com>
2020-02-12 19:23:05 +0200

    Use native js for bootstrap and remove JQuery scripts

--- src/js/script.js
+++
@@ -1,4 +0,0 @@
-//= ../../node_modules/bootstrap/js/dist/util.js
-//= ../../node_modules/bootstrap/js/dist/modal.js
-//= ../../node_modules/bootstrap/js/dist/modal.js
-//= ../../node_modules/bootstrap/js/dist/carousel.js
